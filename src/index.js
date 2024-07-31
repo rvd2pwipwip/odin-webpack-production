c5e588ee8e1914c6694cc88df6b140e079885e65
Herve Desrosiers <herve_desrosiers@hotmail.com>
2024-07-31 14:59:38 -0400

    webpack asset management with css loaders, csv/xml loaders and assets images/fonts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import _ from "lodash";
+import _ from 'lodash';
+import './style.css';
+import Bckg from './thumbnail.png';
+import Note from './data.xml';
+import Channels from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const myBckg = new Image();
+  myBckg.src = Bckg;
+  element.appendChild(myBckg);
+
+  console.log(Note);
+  console.log(Channels);
 
   return element;
 }
